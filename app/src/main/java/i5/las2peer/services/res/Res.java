ded1a3b7e4c2070962a4844ff1b39405ef98f6f2
PhilCAEUser <mail@rwth>
2022-06-10 17:52:00 +0000

    Http Method[gettest] edited by Anonymous

--- app/src/main/java/i5/las2peer/services/res/Res.java
+++ app/src/main/java/i5/las2peer/services/res/Res.java
@@ -117,7 +117,8 @@ public class Res extends RESTService {
     boolean res_condition = true;
     if(res_condition) {
       JSONObject result = new JSONObject();
-      result.put("id", 1321); 
+      result.put("id", 1321);  
+      result.put("id2", 1321); 
       
 
       return Response.status(HttpURLConnection.HTTP_OK).entity(result.toJSONString()).build();
